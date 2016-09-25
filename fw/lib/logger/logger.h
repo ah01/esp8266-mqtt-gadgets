41cbabace4afb9aa7ef1e1d82bff39034e88de1b
ah01 <horcicaa@gmail.com>
2016-09-25 22:08:12 +0200

    Use PROGMEM for logger

--- fw/lib/logger/logger.h
+++ fw/lib/logger/logger.h
@@ -1,29 +1,35 @@
 #ifndef _logger_h_
 #define _logger_h_
 
-//#include <Arduino.h>
+#include <Arduino.h>
+
+const char LOGGER_CORE_PREFIX[] PROGMEM = "C: ";
+const char LOGGER_MODULE_PREFIX[] PROGMEM = "M: ";
 
 #define LOGGER_STREAM Serial
 
+// --- Logger for MODULES ---
+
 #define LOG(txt) \
-    LOGGER_STREAM.print("M: ");\
-    LOGGER_STREAM.println(txt);
+    LOGGER_STREAM.print(FPSTR(LOGGER_CORE_PREFIX));\
+    LOGGER_STREAM.println(F(txt));
 
 #define LOGV(txt, value) \
-    LOGGER_STREAM.print("M: ");\
-    LOGGER_STREAM.print(txt);\
-    LOGGER_STREAM.print(" ");\
+    LOGGER_STREAM.print(FPSTR(LOGGER_CORE_PREFIX));\
+    LOGGER_STREAM.print(F(txt));\
+    LOGGER_STREAM.print(F(" "));\
     LOGGER_STREAM.println(value);
 
+// --- Logger for CORE ---
 
 #define CORE_LOG(txt) \
-    LOGGER_STREAM.print("C: ");\
-    LOGGER_STREAM.println(txt);
+    LOGGER_STREAM.print(FPSTR(LOGGER_MODULE_PREFIX));\
+    LOGGER_STREAM.println(F(txt));
 
 #define CORE_LOGV(txt, value) \
-    LOGGER_STREAM.print("C: ");\
-    LOGGER_STREAM.print(txt);\
-    LOGGER_STREAM.print(" ");\
+    LOGGER_STREAM.print(FPSTR(LOGGER_MODULE_PREFIX));\
+    LOGGER_STREAM.print(F(txt));\
+    LOGGER_STREAM.print(F(" "));\
     LOGGER_STREAM.println(value);
 
 #endif
